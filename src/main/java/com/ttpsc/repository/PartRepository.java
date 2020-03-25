2267bc4d4fd5786a819d119df8cb9107f1c419c3
Jacek Sawko <jacek.sawko@ttpsc.pl>
2020-03-25 19:50:54 +0100

    added spring data jpa dependency and Repos

---
+++ src/main/java/com/ttpsc/repository/PartRepository.java
@@ -0,0 +1,8 @@
+package com.ttpsc.repository;
+
+import com.ttpsc.entity.Part;
+import org.springframework.data.repository.CrudRepository;
+
+public interface PartRepository extends CrudRepository<Part, Long> {
+
+}
