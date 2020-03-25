62809f47e79ee196f0038e1a4d8da879d2e86d4d
Jacek Sawko <jacek.sawko@ttpsc.pl>
2020-03-25 22:48:43 +0100

    added constructors to entities and @Data

--- src/main/java/com/ttpsc/entity/Car.java
+++ src/main/java/com/ttpsc/entity/Car.java
@@ -1,9 +1,12 @@
 package com.ttpsc.entity;
 
+import lombok.Data;
+
 import javax.persistence.*;
 import java.math.BigDecimal;
 import java.util.Set;
 
+@Data
 @Entity
 public class Car {
 
@@ -20,4 +23,11 @@ public class Car {
     @OneToMany(mappedBy = "car", cascade = {CascadeType.DETACH, CascadeType.MERGE, CascadeType.PERSIST, CascadeType.REFRESH})
     private Set<Part> parts;
 
+    public Car() {}
+
+    public Car(String name, BigDecimal price, String color) {
+        this.name = name;
+        this.price = price;
+        this.color = color;
+    }
 }
--- src/main/java/com/ttpsc/entity/Part.java
+++ src/main/java/com/ttpsc/entity/Part.java
@@ -1,8 +1,11 @@
 package com.ttpsc.entity;
 
+import lombok.Data;
+
 import javax.persistence.*;
 import java.math.BigDecimal;
 
+@Data
 @Entity
 public class Part {
 
@@ -16,7 +19,15 @@ public class Part {
 
     private Integer quantity;
 
-    @ManyToOne
+    @ManyToOne(cascade = {CascadeType.DETACH, CascadeType.MERGE, CascadeType.PERSIST, CascadeType.REFRESH})
     @JoinColumn(name = "car_id", nullable = false)
     private Car car;
+
+    public Part() {}
+
+    public Part(String name, BigDecimal price, Integer quantity) {
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+    }
 }
