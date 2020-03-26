8b5826b6e62a698fe659447423dbd0c7aa0d1385
Jacek Sawko <jacek.sawko@ttpsc.pl>
2020-03-26 23:38:05 +0100

    removed lombok, it is not working properly with quarkus, specially upon restart

--- src/main/java/com/ttpsc/entity/Car.java
+++ src/main/java/com/ttpsc/entity/Car.java
@@ -1,13 +1,13 @@
 package com.ttpsc.entity;
 
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
 import javax.persistence.*;
+import javax.validation.constraints.Positive;
+import javax.xml.bind.annotation.XmlRootElement;
 import java.math.BigDecimal;
+import java.util.Objects;
 
-@Data
-@NoArgsConstructor
+//@Data
+@XmlRootElement
 @Entity
 public class Car {
 
@@ -18,7 +18,7 @@ public class Car {
     @Column(nullable = false, unique = true)
     private String name;
 
-//    @PositiveOrZero
+    @Positive
     @Column(nullable = false)
     private BigDecimal price;
 
@@ -29,5 +29,61 @@ public class Car {
         this.name = name;
         this.price = price;
         this.color = color;
+    }
+
+    public Car() {}
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Car car = (Car) o;
+
+        if (!Objects.equals(id, car.id)) return false;
+        if (!Objects.equals(name, car.name)) return false;
+        if (!Objects.equals(price, car.price)) return false;
+        return Objects.equals(color, car.color);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id != null ? id.hashCode() : 0;
+        result = 31 * result + (name != null ? name.hashCode() : 0);
+        result = 31 * result + (price != null ? price.hashCode() : 0);
+        result = 31 * result + (color != null ? color.hashCode() : 0);
+        return result;
     }
 }
